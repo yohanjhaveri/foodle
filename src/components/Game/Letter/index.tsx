b884c7efa837fe084ba64e95001dc7ffa2e04c21
Yohan Jhaveri <yohanjhaveri@gmail.com>
2023-03-16 14:02:15 -0400

    Refactor #6

--- src/components/Game/Letter/index.tsx
+++ src/components/Game/Letter/index.tsx
@@ -7,22 +7,20 @@ type LetterProps = {
   letter?: string;
 };
 
-export const Letter = ({ color, letter }: LetterProps) => {
-  return (
-    <Flex
-      justify="center"
-      align="center"
-      width={{ base: "56px", md: "72px" }}
-      height={{ base: "56px", md: "72px" }}
-      fontSize={{ base: "32px", md: "36px" }}
-      fontWeight="bold"
-      color="white"
-      borderWidth="2px"
-      borderStyle="solid"
-      borderColor={color ? getColorValue(color) : "gray.600"}
-      background={color && getColorValue(color)}
-    >
-      {letter}
-    </Flex>
-  );
-};
+export const Letter = ({ color, letter }: LetterProps) => (
+  <Flex
+    justify="center"
+    align="center"
+    width={{ base: "56px", md: "72px" }}
+    height={{ base: "56px", md: "72px" }}
+    fontSize={{ base: "32px", md: "36px" }}
+    fontWeight="bold"
+    color="white"
+    borderWidth="2px"
+    borderStyle="solid"
+    borderColor={color ? getColorValue(color) : "gray.600"}
+    background={color && getColorValue(color)}
+  >
+    {letter}
+  </Flex>
+);
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,14 +3,12 @@ import { Game } from "./Game";
 import { Header } from "./Header";
 import { Keyboard } from "./Keyboard";
 
-export const Main = () => {
-  return (
-    <Box h="100%">
-      <Header />
-      <Flex height="calc(100% - 60px)" flexDirection="column">
-        <Game />
-        <Keyboard />
-      </Flex>
-    </Box>
-  );
-};
+export const Main = () => (
+  <Box h="100%">
+    <Header />
+    <Flex height="calc(100% - 60px)" flexDirection="column">
+      <Game />
+      <Keyboard />
+    </Flex>
+  </Box>
+);
