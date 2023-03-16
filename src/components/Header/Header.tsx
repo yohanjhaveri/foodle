88d2a316be5525a5362d5a68ec290ce73c55c3e5
Yohan Jhaveri <yohanjhaveri@gmail.com>
2023-03-15 23:45:53 -0400

    Removed modals

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,8 @@
 import { Flex, Image } from "@chakra-ui/react";
-import { FaChartBar, FaCog, FaRegQuestionCircle } from "react-icons/fa";
-
-import { ModalName } from "../../types";
-import { HeaderIcon } from "./HeaderIcon";
 
 import FoodleLogo from "../../images/foodle-logo.png";
 
-type Props = {
-  handleOpenModal: (modal: ModalName) => void;
-};
-
-export const Header = ({ handleOpenModal }: Props) => {
-  const LEFT = (
-    <Flex margin="20px" gap="20px">
-      <HeaderIcon
-        icon={<FaRegQuestionCircle />}
-        ariaLabel="about"
-        onClick={() => handleOpenModal("ABOUT")}
-      />
-      <HeaderIcon
-        icon={<Flex width="20px" />}
-        ariaLabel="dummy"
-        tabIndex={-1}
-      />
-    </Flex>
-  );
-
-  const LOGO = <Image src={FoodleLogo} height="60px" />;
-
-  const RIGHT = (
-    <Flex margin="20px" gap="20px">
-      <HeaderIcon
-        icon={<FaChartBar />}
-        ariaLabel="stats"
-        onClick={() => handleOpenModal("STATS")}
-      />
-      <HeaderIcon
-        icon={<FaCog />}
-        ariaLabel="settings"
-        onClick={() => handleOpenModal("SETTINGS")}
-      />
-    </Flex>
-  );
-
+export const Header = () => {
   return (
     <Flex
       width="100%"
@@ -50,14 +10,12 @@ export const Header = ({ handleOpenModal }: Props) => {
       borderBottomStyle="solid"
       borderBottomColor="gray.700"
       background="gray.800"
-      justify="space-between"
+      justify="center"
       align="center"
       position="fixed"
       top="0"
     >
-      {LEFT}
-      {LOGO}
-      {RIGHT}
+      <Image src={FoodleLogo} height="60px" />
     </Flex>
   );
 };
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,71 +1,16 @@
-import {
-  Box,
-  Grid,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { Box, Grid } from "@chakra-ui/react";
 import { useGame } from "../hooks/useGame";
-import { ModalName } from "../types";
 import { Game } from "./Game/Game";
 import { Header } from "./Header/Header";
 import { Keyboard } from "./Keyboard/Keyboard";
-import { AboutModal } from "./Modals/AboutModal";
-import { SettingsModal } from "./Modals/SettingsModal";
-import { StatsModal } from "./Modals/StatsModal";
 
 export const Main = () => {
   const { state } = useGame();
 
-  const [openModal, setOpenModal] = useState<ModalName | "">("");
-
-  const handleCloseModal = () => {
-    setOpenModal("");
-  };
-
-  const handleOpenModal = (modal: ModalName) => {
-    setOpenModal(modal);
-  };
-
-  useEffect(() => {
-    if (["WIN", "LOSE"].includes(state)) {
-      handleOpenModal("STATS");
-    }
-  }, [state]);
-
-  const heading =
-    openModal &&
-    {
-      STATS: "STATISTICS",
-      ABOUT: "HOW TO PLAY",
-      SETTINGS: "SETTINGS",
-    }[openModal];
-
-  const MODAL =
-    openModal &&
-    {
-      STATS: <StatsModal />,
-      ABOUT: <AboutModal />,
-      SETTINGS: <SettingsModal />,
-    }[openModal];
-
   return (
     <Box minHeight="100vh">
-      <Modal isOpen={!!openModal} onClose={handleCloseModal}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{heading}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>{MODAL}</ModalBody>
-          <ModalFooter></ModalFooter>
-        </ModalContent>
-      </Modal>
-      <Header handleOpenModal={handleOpenModal} />
+      <Header />
       <Grid
         gap="20px"
         justifyContent="center"
